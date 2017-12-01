20667bc941d2fcb6cf2390222500df3f6a771a6f
Phil Simmonds <phil.simmonds@blake.com.au>
2017-12-01 15:12:28 +1100

    Replace some audio files

--- main.lua
+++ main.lua
@@ -2,25 +2,21 @@ require("game")
 require("inputs")
 
 function love.load()
-  -- mainFont = love.graphics.newFont("resources/fonts/Coolville.ttf", 20)
+  love.mouse.setVisible(false)
+
   mainFont = love.graphics.newFont("resources/fonts/pcsenior.ttf", 13)
   titleFont = love.graphics.newFont("resources/fonts/pcsenior.ttf", 40)
   menuFont = love.graphics.newFont("resources/fonts/pcsenior.ttf", 15)
   smallMenuFont = love.graphics.newFont("resources/fonts/pcsenior.ttf", 12)
 
-  love.mouse.setVisible(false)
-
   pingSound = love.audio.newSource("resources/audio/ping.wav", "static")
   gameOverSound = love.audio.newSource("resources/audio/gameOver.wav", "static")
-  -- menuMusic = love.audio.newSource("resources/audio/menu.wav", "static")
-  menuMusic = love.audio.newSource("resources/audio/menu5.wav", "static")
-  -- menuMusic2 = love.audio.newSource("resources/audio/menu3.wav", "static")
+  menuMusic = love.audio.newSource("resources/audio/menu.wav", "static")
   playingMusic = love.audio.newSource("resources/audio/playing.wav", "static")
   menuMusic:setLooping(true)
   playingMusic:setLooping(true)
 
   menuMusic:play()
-  -- menuMusic2:play()
 end
 
 function love.draw()
--- menu.lua
+++ menu.lua
@@ -19,12 +19,17 @@ function menu:draw()
   love.graphics.printf("\"And use Space to detect them on our sonar!\"", 0, h/2.8+40, w, "center")
 
   love.graphics.setFont(smallMenuFont)
-  love.graphics.printf("\"Oh and by the way we found this minor glitch in our systems where for some reason our engines go faster and faster each time we ping our sonar..\"", 0, h/2.8+80, w, "center")
-  love.graphics.printf("\"but dont worry about that..\"", 0, h/2.8+120, w, "center")
+  -- love.graphics.printf("\"Oh and by the way we found this minor glitch in our systems where for some reason\"", 0, h/2.8+80, w, "center")
+  -- love.graphics.printf("\"our engines go faster and faster each time we ping our sonar..\"", 0, h/2.8+100, w, "center")
+  -- love.graphics.printf("\"but dont worry about that..\"", 0, h/2.8+120, w, "center")
+  love.graphics.printf("\"Oh by the way..\"", 0, h/2.8+80, w, "center")
+  love.graphics.printf("\"We have this glitch in our system when we use sonar", 0, h/2.8+100, w, "center")
+  love.graphics.printf("the engines go faster and faster..\"", 0, h/2.8+120, w, "center")
+  love.graphics.printf("\"But don't worry about that..\"", 0, h/2.8+140, w, "center")
 
   love.graphics.setFont(menuFont)
-  love.graphics.printf("High Score: " .. math.floor(game.highscore), 0, h/2.8+160, w, "center")
-  love.graphics.printf("Press F to toggle fullscreen", 0, h/2.8+180, w, "center")
-  love.graphics.printf("Press Enter to play or Esc to quit anytime", 0, h/2.8+200, w, "center")
+  love.graphics.printf("High Score: " .. math.floor(game.highscore), 0, h/2.8+180, w, "center")
+  love.graphics.printf("Press F to toggle fullscreen", 0, h/2.8+200, w, "center")
+  love.graphics.printf("Press Enter to play or Esc to quit anytime", 0, h/2.8+220, w, "center")
 end
 
